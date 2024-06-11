e75d9e0aaf37cc7538eeee44d32c3f23a3882ab8
Mohamed Ghoul <118106616+Mohavvvvd@users.noreply.github.com>
2024-06-11 05:31:02 -0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var elements = document.querySelectorAll('.a');
+    const hb = document.querySelectorAll(".btv");
     elements.forEach(function(element) {
         if (element.classList.contains("a")) {
             element.classList.remove('hidden');
             element.classList.add('visible');
         }
     });
+    hb.forEach(function(b){
+        b.classList.remove("h");
+    })
+    hb[0].classList.add("h");
     var scrol = document.querySelectorAll(".x");
     scrol.forEach(function(button) {
         button.addEventListener("click", function() {
@@ -22,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const ct = document.querySelector(".ct");
 
     btn.addEventListener("click", function() {
+        hb[1].classList.add("h");
+        hb[0].classList.remove("h");
+        hb[2].classList.remove("h");
         hideElement(".a");
         hideElement(".cont");
         hideElement(".soc");
@@ -32,6 +40,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     ap.addEventListener("click", function() {
+        hb[1].classList.add("h");
+        hb[0].classList.remove("h");
+        hb[2].classList.remove("h");
         hideElement(".a");
         hideElement(".cont");
         hideElement(".soc");
@@ -42,6 +53,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     acc.addEventListener("click", function() {
+        hb[0].classList.add("h");
+        hb[1].classList.remove("h");
+        hb[2].classList.remove("h");
         showElement(".a");
         hideElement(".appli");
         hideElement(".s");
@@ -52,6 +66,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     ct.addEventListener("click", function() {
+        hb[2].classList.add("h");
+        hb[1].classList.remove("h");
+        hb[0].classList.remove("h");
         showElement(".cont");
         showElement(".soc");
         hideElement(".a");
